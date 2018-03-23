cf9becd9756610b27d9b6b699c4c1f64b94150ce
Karl Köörna <karlkoorna@gmail.com>
2018-03-23 19:28:34 +0200

    Initial commit (vscode)

---
+++ vscode/extensions.sh
@@ -0,0 +1,15 @@
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+code --install-extension eg2.vscode-npm-script
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension jvitor83.typings-autoinstaller
+code --install-extension lukehoban.Go
+code --install-extension ms-python.python
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension shardulm94.trailing-spaces
+code --install-extension shinnn.stylelint
+code --install-extension slevesque.vscode-autohotkey
+code --install-extension Tyriar.sort-lines
+code --install-extension wayou.vscode-todo-highlight
