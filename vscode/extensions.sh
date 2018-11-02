e68c9f83644c1d5ae63c9b31121a56df3e27b38b
Karl Köörna <karlkoorna@gmail.com>
2018-11-02 21:54:13 +0200

    Update extensions.sh

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -5,6 +5,7 @@ code --install-extension eg2.vscode-npm-script
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension jvitor83.typings-autoinstaller
+code --install-extension mechatroner.rainbow-csv
 code --install-extension mikestead.dotenv
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension shardulm94.trailing-spaces
