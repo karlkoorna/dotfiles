5b9850b66f4fdda689275ccc286978e65c03bef6
karlkoorna <karlkoorna@gmail.com>
2021-04-03 19:55:03 +0300

    Update oto2mb.js

--- scripts/userscripts/oto2mb.js
+++ scripts/userscripts/oto2mb.js
@@ -1,20 +1,32 @@
 // ==UserScript==
-// @name         Ototoy to MusicBrainz tracklist
-// @description  Copy MusicBrainz tracklist from Ototoy release page with CTRL+C.
+// @name         OTO2MB
+// @description  Copy tracklist for MusicBrainz from Ototoy release page.
 // @author       Karl Köörna
 // @version      1.0.0
 // @match        https://ototoy.jp/_/default/p/*
 // @grant        GM.setClipboard
+// @grant        GM.addStyle
 // ==/UserScript==
 
-const ARTIST_SEP = '---';
+const ARTIST_SEP = '\t';
 
-addEventListener('keydown', (e) => {
-	if (!e.ctrlKey || e.key !== 'c') return;
-	if (getSelection().focusOffset !== 0) return;
-	
+const button = document.createElement('a');
+button.id = 'oto2mb';
+button.innerText = 'Copy for MB';
+
+button.addEventListener('click', () => {
 	GM.setClipboard(Array.from(document.querySelectorAll('.tracklist tr:not(:first-child)')).map((el) => {
-		const elPlay = el.querySelector('button[trackcode]');
-		return `${elPlay.getAttribute('tn').toString().padStart(2, '0')}. ${elPlay.getAttribute('artist')} ${ARTIST_SEP} ${elPlay.getAttribute('title')} (${el.children[2].innerText})`
+		const info = el.querySelector('button[trackcode]');
+		return `${info.getAttribute('tn').toString().padStart(2, '0')}. ${info.getAttribute('artist')} ${ARTIST_SEP} ${info.getAttribute('title')} (${el.children[2].innerText})`
 	}).join('\n'));
 });
+
+document.querySelector('.tracklist tr:first-child > th:nth-child(2)').appendChild(button);
+
+GM.addStyle(`
+#oto2mb {
+	margin-left: 12px;
+	font-size: 12px;
+	cursor: pointer;
+}
+`);
