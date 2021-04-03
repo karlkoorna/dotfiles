347e313b107ff5b4518c0d2af8733cf280da3ce8
karlkoorna <karlkoorna@gmail.com>
2021-04-03 21:53:01 +0300

    Update mboit.js

--- scripts/userscripts/mboit.js
+++ scripts/userscripts/mboit.js
@@ -8,7 +8,7 @@
 
 if (!document.querySelector('.releaseheader > .tagger-icon')) {
 	document.querySelector('.releaseheader').insertAdjacentHTML('afterbegin', `
-		<a class="tagger-icon" href="http://127.0.0.1:8000/openalbum?id=${location.href.slice(9)}" title="Open in tagger">
+		<a class="tagger-icon" href="http://127.0.0.1:8000/openalbum?id=${location.pathname.slice(9)}" title="Open in tagger">
 			<img alt="Tagger" src="https://staticbrainz.org/MB/mblookup-tagger-b8fe559.png">
 		</a>
 	`);
