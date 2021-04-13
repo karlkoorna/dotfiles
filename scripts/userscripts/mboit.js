703068a8b61963a92a2d70bbe5886f98ec4265f3
karlkoorna <karlkoorna@gmail.com>
2021-04-13 20:55:58 +0300

    Update mboit.js

--- scripts/userscripts/mboit.js
+++ scripts/userscripts/mboit.js
@@ -21,7 +21,7 @@ if (location.pathname.startsWith('/release/')) {
 	for (const el of document.querySelectorAll('th[colspan]')) el.colSpan++;
 	for (const el of document.querySelectorAll('.odd, .even')) el.innerHTML += `
 		<td>
-			<a class="tagger-icon" href="http://127.0.0.1:8080/openalbum?id=${el.querySelector('a[href^="/release/"]').href.replace('/release/', '').replace('/cover-art', '')}" title="Open in tagger">
+			<a class="tagger-icon" href="http://127.0.0.1:8000/openalbum?id=${el.querySelector('a[href^="/release/"]').getAttribute('href').replace('/release/', '').replace('/cover-art', '')}" title="Open in tagger">
 				<img alt="Tagger" src="https://staticbrainz.org/MB/mblookup-tagger-b8fe559.png">
 			</a>
 		</td>
