631bcb4a074d46b20d00c9c88939126db487997d
karlkoorna <karlkoorna@gmail.com>
2022-01-01 13:56:39 +0200

    Update

--- scripts/userscripts/mboit.js
+++ scripts/userscripts/mboit.js
@@ -13,7 +13,7 @@ if (document.querySelector('.tagger-icon')) return;
 if (location.pathname.startsWith('/release/')) {
 	document.querySelector('.releaseheader').insertAdjacentHTML('afterbegin', `
 		<a class="tagger-icon" href="http://127.0.0.1:8000/openalbum?id=${location.pathname.slice(9)}" title="Open in tagger">
-			<img alt="Tagger" src="https://staticbrainz.org/MB/mblookup-tagger-b8fe559.png">
+			<img alt="Tagger" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAANCAMAAAADg7fkAAAAVFBMVEX////////j4+Nm/5nW1dXMzMzMy6zDw6e6ubmzs56sq6uqq5ihopOfoaSZmZmXmIyNj4WEhX17fHV2c3RmZmZaV1hVVlhMSUpDQ0M/OzwzMzMAAABK1i8nAAAAAXRSTlMAQObYZgAAAIZJREFUeNqNkdsOhCAMRHHVesEF8e74//+pbYjEfXA7KfNQToaSGlG7H3/FXLFunkL+LiYdApGGHNGQiqzR+LkX8hqGLU/GLT5C7rbDd+kl84jFN9FS14CCy9x0k5eYFZNKmZ2Fg//NTCYe57TB04N8zHm/blpY3d8ZHSodaT4zNDsSlYq9n5odFv9PkEJKAAAAAElFTkSuQmCC">
 		</a>
 	`);
 } else {
@@ -22,7 +22,7 @@ if (location.pathname.startsWith('/release/')) {
 	for (const el of document.querySelectorAll('.odd, .even')) el.innerHTML += `
 		<td>
 			<a class="tagger-icon" href="http://127.0.0.1:8000/openalbum?id=${el.querySelector('a[href^="/release/"]').getAttribute('href').replace('/release/', '').replace('/cover-art', '')}" title="Open in tagger">
-				<img alt="Tagger" src="https://staticbrainz.org/MB/mblookup-tagger-b8fe559.png">
+				<img alt="Tagger" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAANCAMAAAADg7fkAAAAVFBMVEX////////j4+Nm/5nW1dXMzMzMy6zDw6e6ubmzs56sq6uqq5ihopOfoaSZmZmXmIyNj4WEhX17fHV2c3RmZmZaV1hVVlhMSUpDQ0M/OzwzMzMAAABK1i8nAAAAAXRSTlMAQObYZgAAAIZJREFUeNqNkdsOhCAMRHHVesEF8e74//+pbYjEfXA7KfNQToaSGlG7H3/FXLFunkL+LiYdApGGHNGQiqzR+LkX8hqGLU/GLT5C7rbDd+kl84jFN9FS14CCy9x0k5eYFZNKmZ2Fg//NTCYe57TB04N8zHm/blpY3d8ZHSodaT4zNDsSlYq9n5odFv9PkEJKAAAAAElFTkSuQmCC">
 			</a>
 		</td>
 	`;
